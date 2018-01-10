61c07cd40c489af568c8724f61e30152e51722d0
jianggle <599580031@qq.com>
2018-01-10 10:29:01 +0800

    使下载模型可按需安装

---
+++ install_package/phpcms/modules/addxiazai/install/config.inc.php
@@ -0,0 +1,10 @@
+<?php 
+defined('IN_PHPCMS') or exit('Access Denied');
+defined('INSTALL') or exit('Access Denied');
+$module = 'addxiazai';
+$modulename = '添加下载模型';
+$introduce = '仅用于添加“下载模型”咯！若要删除，进入“内容-模型管理-下载模型”删除即可，一起飞工作室出品！';
+$author = 'Eqifei Studio';
+$authorsite = 'http://www.eqifei.net';
+$authoremail = '599580031@qq.com';
+?>
