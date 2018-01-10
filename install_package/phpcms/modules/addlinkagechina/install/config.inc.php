bf1ac5111a623cbc7a7b481bd0001deb4b7ab474
jianggle <599580031@qq.com>
2018-01-10 09:48:37 +0800

    在模块管理处按需安装默认的省市区数据以减轻数据库压力

---
+++ install_package/phpcms/modules/addlinkagechina/install/config.inc.php
@@ -0,0 +1,10 @@
+<?php 
+defined('IN_PHPCMS') or exit('Access Denied');
+defined('INSTALL') or exit('Access Denied');
+$module = 'addlinkagechina';
+$modulename = '添加联动菜单-中国省份';
+$introduce = '当不止使用此联动菜单时，请一定要先安装此模块，因为如果先添加了其他的菜单则会导致表中的id出错！';
+$author = 'Eqifei Studio';
+$authorsite = 'http://www.eqifei.net';
+$authoremail = '599580031@qq.com';
+?>
